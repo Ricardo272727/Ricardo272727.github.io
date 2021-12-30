925ff8ac7b886047da1b0034785f5764d623177f
Ricardo Cuanalo <cuanaloricardo@gmail.com>
2021-12-30 14:19:24 -0600

    version 2021 builded

---
+++ precache-manifest.709f3edc07f4891e38c9e595e00ae354.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "610684b7a84db7300b343052612f870f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d42b2120f23a94854e70",
+    "url": "/static/css/2.485d0daa.chunk.css"
+  },
+  {
+    "revision": "34682349934e22b81bbf",
+    "url": "/static/css/main.0da45cba.chunk.css"
+  },
+  {
+    "revision": "d42b2120f23a94854e70",
+    "url": "/static/js/2.758799d9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.758799d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34682349934e22b81bbf",
+    "url": "/static/js/main.606dcdf4.chunk.js"
+  },
+  {
+    "revision": "dfe3d817a9ac0c05d2c9",
+    "url": "/static/js/runtime-main.84cfcdfc.js"
+  },
+  {
+    "revision": "259d4afad7edca07e727ef80f5bbce07",
+    "url": "/static/media/Bungee-Regular.259d4afa.ttf"
+  }
+]);
